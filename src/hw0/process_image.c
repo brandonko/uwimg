cf8354650361748da576db24b35e2e1efbce69dc
Brandon Ko <brandonko@Brandons-MacBook-Pro.local>
2021-01-15 16:08:25 -0800

    finished hw0

--- src/hw0/process_image.c
+++ src/hw0/process_image.c
@@ -93,11 +93,108 @@ float three_way_min(float a, float b, float c)
 void rgb_to_hsv(image im)
 {
     // TODO Fill this in
-    
+    for (int i = 0; i < im.h; i++) {
+        for (int j = 0; j < im.w; j++) {
+            float r = get_pixel(im, j, i, 0);
+            float g = get_pixel(im, j, i, 1);
+            float b = get_pixel(im, j, i, 2);
+
+            float V = three_way_max(r,g,b);
+            float m = three_way_min(r, g, b);
+            float C = V - m;
+            float S = V == 0 ? 0 : C / V;
+
+            float H_prime = 0;
+            float H = 0;
+
+            if (C != 0) {
+                if (V == r) {
+                    H_prime = (g - b) / C;
+                } else if (V == g) {
+                    H_prime = ((b - r) / C) + 2;
+                } else if (V == b) {
+                    H_prime = ((r - g) / C ) + 4;
+                }
+
+                if (H_prime < 0) {
+                    H = (H_prime / 6) + 1;
+                } else {
+                    H = H_prime / 6;
+                }
+            }
+
+            set_pixel(im, j, i, 0, H);
+            set_pixel(im, j, i, 1, S);
+            set_pixel(im, j, i, 2, V);
+        }
+    }
 }
 
 void hsv_to_rgb(image im)
 {
     // TODO Fill this in
+    for (int i = 0; i < im.h; i++) {
+        for (int j = 0; j < im.w; j++) {
+            float H = get_pixel(im, j, i, 0);
+            float S = get_pixel(im, j, i, 1);
+            float V = get_pixel(im, j, i, 2);
+
+            float R = 0;
+            float G = 0;
+            float B = 0;
+
+            if (S <= 0) {
+                R = V;
+                G = V;
+                B = V;
+            } else {
+                if (H > 1) H = 0;
+                float HH = H * 6;
+                long i = (long) HH;
+                float FF = HH - i;
+                float p = V * (1.0 - S);
+                float q = V * (1.0 - (S * FF));
+                float t = V * (1.0 - (S * (1.0 - FF)));
+
+                switch (i) {
+                    case 0:
+                        R = V;
+                        G = t;
+                        B = p;
+                        break;
+                    case 1:
+                        R = q;
+                        G = V;
+                        B = p;
+                        break;
+                    case 2:
+                        R = p;
+                        G = V;
+                        B = t;
+                        break;
+                    case 3:
+                        R = p;
+                        G = q;
+                        B = V;
+                        break;
+                    case 4:
+                        R = t;
+                        G = p;
+                        B = V;
+                        break;
+                    case 5:
+                    default:
+                        R = V;
+                        G = p;
+                        B = q;
+                        break;
+                }
+            }
+
+            set_pixel(im, j, i, 0, R);
+            set_pixel(im, j, i, 1, G);
+            set_pixel(im, j, i, 2, B);
+        }
+    }
 }
 
